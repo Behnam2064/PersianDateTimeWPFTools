dee3766ea3041985bfe029d9d8c37f68b93cd110
Behnam2064 <Behnam@DESKTOP-0PJLIOQ>
2025-03-19 11:41:05 +0330

    chore:a little change in test window

--- PersianDateTimeWPFTools/TestPersianCalendar/App.xaml.cs
+++ PersianDateTimeWPFTools/TestPersianCalendar/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Windows;
 
 namespace TestPersianCalendar
@@ -9,6 +10,22 @@ namespace TestPersianCalendar
     /// </summary>
     public partial class App : Application
     {
+        private bool IsUsCuture = true;
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            /*if (IsUsCuture)
+            {
+                CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+                CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
+            }
+            else
+            {
+                CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("fa-IR");
+                CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("fa-IR");
+            }
+
+            Thread.CurrentThread.CurrentCulture = CultureInfo.DefaultThreadCurrentCulture;*/
+        }
     }
 
 }
