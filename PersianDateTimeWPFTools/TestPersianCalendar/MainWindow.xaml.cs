44e46ae5bc6a977c45024e908dcb2346c343561a
Behnam2064 <Behnam@DESKTOP-0PJLIOQ>
2025-04-05 22:10:29 +0330

    chore:The test project has been changed

--- PersianDateTimeWPFTools/TestPersianCalendar/MainWindow.xaml.cs
+++ PersianDateTimeWPFTools/TestPersianCalendar/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Globalization;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
@@ -20,9 +21,31 @@ namespace TestPersianCalendar
         public MainWindow()
         {
             InitializeComponent();
-            pcwc1.BlackoutDates.Add(new PersianDateTimeWPFTools.Windows.Controls.CalendarDateRange(DateTime.Now.AddDays(2), DateTime.Now.AddDays(2+2)));
+            pcwc1.BlackoutDates.Add(new PersianDateTimeWPFTools.Windows.Controls.CalendarDateRange(DateTime.Now.AddDays(2), DateTime.Now.AddDays(2 + 2)));
             pc1.BlackoutDates.Add(pcwc1.BlackoutDates[0]);
             pdp1.BlackoutDates.Add(pcwc1.BlackoutDates[0]);
+            pdtp1.BlackoutDates.Add(pcwc1.BlackoutDates[0]);
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var isChecked = (sender as ToggleButton)?.IsChecked ?? true;
+            CultureInfo? culture = null;
+            if (isChecked)
+            {
+                culture = CultureInfo.CreateSpecificCulture("en-US");
+            }
+            else
+            {
+
+                culture = CultureInfo.CreateSpecificCulture("fa-IR");
+            }
+
+            pcwc1.CustomCulture = culture;
+            pc1.CustomCulture = culture;
+            pdp1.CustomCulture = culture;
+            pdtp1.CustomCulture = culture;
+
         }
     }
 }
